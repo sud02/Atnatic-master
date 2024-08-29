02fc4406a7c062271329dc78dbeefbbfec514777
Sai <109896677+GowthamVukoti@users.noreply.github.com>
2024-08-29 08:14:46 +0530

    added product slider for mobile devices

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ProductCarousel from './ProductCarousel';
+import ProductSlider from './ProductPage/ProductSlider';
 
 const ProductPage = () => {
   return (
     <div className="product-page">
       {/* Other product page content */}
       <ProductCarousel />
+      <ProductSlider/>
       {/* Other product page content */}
     </div>
   );
---
+++ src/components/ProductPage/ProductSlider.js
@@ -0,0 +1,4 @@
+import React, { useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './ProductSlider.css';
